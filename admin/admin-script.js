6ceeb01f585dc2efdc35238ed2df710efa965d37
ankush-github-11 <ankush10yt@gmail.com>
2025-04-28 22:18:43 +0530

    Designing the Admin Page

---
+++ admin/admin-script.js
@@ -0,0 +1,23 @@
+const initializeTheme = () => {
+    const userPreference = localStorage.getItem('theme'); 
+    const systemPreference = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'; 
+    const theme = userPreference || systemPreference;
+    if (theme === 'dark'){
+        document.body.classList.add('darkmode');
+        document.querySelector('.dark-mode-svg').classList.remove('hidden');
+        document.querySelector('.light-mode-svg').classList.add('hidden'); 
+    }
+    else{
+        document.body.classList.remove('darkmode');
+        document.querySelector('.dark-mode-svg').classList.add('hidden');
+        document.querySelector('.light-mode-svg').classList.remove('hidden'); 
+    }
+}
+initializeTheme();
+document.querySelector('.light-dark-div').addEventListener('click', function () {
+    document.body.classList.toggle('darkmode');
+    const isDarkMode = document.body.classList.contains('darkmode');
+    document.querySelector('.dark-mode-svg').classList.toggle('hidden');
+    document.querySelector('.light-mode-svg').classList.toggle('hidden');
+    localStorage.setItem('theme', isDarkMode ? 'dark' : 'light');
+});
