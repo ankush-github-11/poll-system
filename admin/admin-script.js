c0fb135563593beb0a07f994d93a1c6776fb5faa
ankush-github-11 <ankush10yt@gmail.com>
2025-05-01 16:57:02 +0530

    Updating the Admin Page

--- admin/admin-script.js
+++ admin/admin-script.js
@@ -21,3 +21,21 @@ document.querySelector('.light-dark-div').addEventListener('click', function () 
     document.querySelector('.light-mode-svg').classList.toggle('hidden');
     localStorage.setItem('theme', isDarkMode ? 'dark' : 'light');
 });
+const totalUsers = document.querySelector(".total-users-content").textContent.trim().split("<(&*#$*-)>");
+document.querySelector(".option-1-val").textContent = totalUsers.length - 1;
+for(let i = 0; i < totalUsers.length; i++){
+    const arr = totalUsers[i].split("<-/*756-=-=>");
+    const html = `
+        <div class="total-users-row">
+            <div class="div-11">${i + 1}</div>
+            <div class="div-12">${arr[0]}</div>
+            <div class="div-13">${arr[1]}</div>
+            <div class="div-14">${arr[2]}</div>
+            <div class="div-15">${arr[3]}</div>
+            <div class="div-16">${arr[4]}</div>
+            <div class="div-17">${arr[5]}</div>
+        </div>
+    `;
+    document.querySelector('.total-users').insertAdjacentHTML("beforeend", html);
+    if(i == totalUsers.length - 2) break;
+}
