672a68fb6d11d89312b47394a2f74e3bcba6b0fb
ankush-github-11 <ankush10yt@gmail.com>
2025-06-11 09:48:15 +0530

    Updating the Poll Creation Page

--- mypoll/poll-script.js
+++ mypoll/poll-script.js
@@ -7,23 +7,12 @@ const initializeTheme = () => {
     const theme = userPreference || systemPreference;
     if (theme === 'dark'){
         document.body.classList.add('darkmode');
-        document.querySelector('.dark-mode-svg').classList.remove('hidden');
-        document.querySelector('.light-mode-svg').classList.add('hidden'); 
     }
     else{
         document.body.classList.remove('darkmode');
-        document.querySelector('.dark-mode-svg').classList.add('hidden');
-        document.querySelector('.light-mode-svg').classList.remove('hidden'); 
     }
 }
 initializeTheme();
-document.querySelector('.light-dark-div').addEventListener('click', function () {
-    document.body.classList.toggle('darkmode');
-    const isDarkMode = document.body.classList.contains('darkmode');
-    document.querySelector('.dark-mode-svg').classList.toggle('hidden');
-    document.querySelector('.light-mode-svg').classList.toggle('hidden');
-    localStorage.setItem('theme', isDarkMode ? 'dark' : 'light');
-});
 document.querySelector('.clipboard-btn-1').addEventListener('click', function(){
     document.querySelector('.clipboard-btn-1').classList.add('pointer-none');
     const text = document.querySelector('.link-print-div-1').textContent.trim();
