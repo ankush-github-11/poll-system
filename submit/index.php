831e9c35ceef5b1fa21fe386ee12e7158418f8bc
ankush-github-11 <ankush10yt@gmail.com>
2025-08-21 18:49:32 +0530

    Fixing uid Error

--- submit/index.php
+++ submit/index.php
@@ -1,9 +1,5 @@
 <?php
     include "../config/connect.php";
-    if(!isset($_SESSION["uid"])){
-        header("Location: ../error/");
-        exit();
-    }
     function getUserIP() {
         if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
             return $_SERVER['HTTP_CLIENT_IP'];
