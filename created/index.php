03a84f9a03c0fd29cfcb8f9b42a0b8b81107ec9d
ankush-github-11 <ankush10yt@gmail.com>
2025-12-26 13:32:49 +0530

    Changing the Header Text of Created Page

--- created/index.php
+++ created/index.php
@@ -25,7 +25,7 @@
     <meta name="description" content="View the total number of polls created, track poll statistics, and monitor overall poll activity in the system.">
     <meta name="keywords" content="poll statistics, number of polls, poll tracking, poll activity, poll analytics">
     <meta name="robots" content="index, follow">
-    <title>Polls Participated</title>
+    <title>Polls Created</title>
     <link rel="icon" type="image/png" href="../images/main-logo.png">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
